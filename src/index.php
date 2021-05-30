fc26bce0d620b4366c6a0da8d9ffbe10008b9dc9
Jogendra Kumar <jogendrajangid1@users.noreply.github.com>
2021-05-30 12:32:09 +0530

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "Hello, World from Docker! <br>";
-echo "Hello world! 1 <br>";
+echo "Hello world! 2 <br>";
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
 
 ?>
